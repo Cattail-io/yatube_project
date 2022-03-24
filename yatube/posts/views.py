f7843c8ac8f5f0a778664177c4c871b9493259db
Vit Che <vit_che@mail.ru>
2022-03-24 16:34:35 +0300

    Вывод информации из базы данных на страницы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,11 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post
+
 def index(request):
-    template = 'posts/index.html'
-    title = "Это главная страница проекта Yatube"
-    context = { 'title': title }
-    return render(request, template, context) 
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 
 # Страница со списком мороженого
